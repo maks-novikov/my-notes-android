7795f180839e5cd302423c35df17449441ad905d
Maksim Novikov <maxnovikov51@gmail.com>
2024-01-01 20:36:54 +0200

    Add crypto functionality

---
+++ app/src/main/java/com/maksim/mynotes/data/crypto/CryptoManager.kt
@@ -0,0 +1,107 @@
+package com.maksim.mynotes.data.crypto
+
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
+import android.util.Base64
+import java.io.InputStream
+import java.io.OutputStream
+import java.lang.IllegalStateException
+import java.nio.ByteBuffer
+import java.security.KeyStore
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
+
+class CryptoManager {
+
+    private val keyStore = KeyStore.getInstance("AndroidKeyStore").apply {
+        load(null)
+    }
+
+    private val encryptCipher = Cipher.getInstance(TRANSFORMATION).apply {
+        init(Cipher.ENCRYPT_MODE, getKey())
+    }
+
+    private fun getDecryptCipher(iv: ByteArray): Cipher {
+        return Cipher.getInstance(TRANSFORMATION).apply {
+            init(Cipher.DECRYPT_MODE, getKey(), IvParameterSpec(iv))
+        }
+    }
+
+    companion object {
+        private const val ALG = KeyProperties.KEY_ALGORITHM_AES
+        private const val BLOCK_MODE = KeyProperties.BLOCK_MODE_CBC
+        private const val PADDING = KeyProperties.ENCRYPTION_PADDING_PKCS7
+        private const val TRANSFORMATION = "$ALG/$BLOCK_MODE/$PADDING"
+        private const val DEFAULT_KEY_ALIAS = "default_key"
+    }
+
+    private fun getKey(): SecretKey {
+        return (keyStore.getEntry(DEFAULT_KEY_ALIAS, null) as? KeyStore.SecretKeyEntry)?.secretKey
+            ?: createKey(DEFAULT_KEY_ALIAS)
+    }
+
+    private fun createKey(alias: String): SecretKey {
+        return KeyGenerator.getInstance(ALG).apply {
+            init(
+                KeyGenParameterSpec.Builder(
+                    alias,
+                    KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT
+                ).setBlockModes(BLOCK_MODE)
+                    .setEncryptionPaddings(PADDING)
+                    .setUserAuthenticationRequired(false)
+                    .setRandomizedEncryptionRequired(true)
+                    .build()
+            )
+        }.generateKey()
+    }
+
+
+    fun encrypt(data: String): String {
+        val encryptedBytes = encryptCipher.doFinal(data.toByteArray())
+
+        val encrypted = Base64.encode(encryptedBytes, Base64.DEFAULT).decodeToString()
+        val iv = Base64.encode(encryptCipher.iv, Base64.DEFAULT).decodeToString()
+        val separator = "|"
+
+        val result = StringBuilder().append(iv).append(separator).append(encrypted)
+
+        return result.toString()
+    }
+
+    fun decrypt(data: String): String {
+        val parts = data.split("|")
+        if (parts.size == 2) {
+            val iv = Base64.decode(parts.first(), Base64.DEFAULT)
+            val encryptedData = Base64.decode(parts.last(), Base64.DEFAULT)
+            return getDecryptCipher(iv).doFinal(encryptedData).decodeToString()
+        } else {
+            throw IllegalStateException("The data is corrupted")
+        }
+    }
+
+    fun encrypt(bytes: ByteArray, outputStream: OutputStream): ByteArray {
+        val encrypted = encryptCipher.doFinal(bytes)
+        outputStream.use {
+            it.write(encryptCipher.iv.size)
+            it.write(encryptCipher.iv)
+            it.write(encrypted.size)
+            it.write(encrypted)
+        }
+        return encrypted
+    }
+
+    fun decrypt(inputStream: InputStream): ByteArray {
+        return inputStream.use {
+            val ivSize = it.read()
+            val iv = ByteArray(ivSize)
+            it.read(iv)
+            val dataSize = it.read()
+            val data = ByteArray(dataSize)
+            it.read(data)
+            getDecryptCipher(iv).doFinal(data)
+        }
+    }
+
+}
--- app/src/main/java/com/maksim/mynotes/ui/auth/AuthActivity.kt
+++ app/src/main/java/com/maksim/mynotes/ui/auth/AuthActivity.kt
@@ -1,10 +1,15 @@
 package com.maksim.mynotes.ui.auth
 
 import android.os.Bundle
+import android.util.Log
+import com.maksim.mynotes.data.crypto.CryptoManager
 
 import com.maksim.mynotes.databinding.ActivityAuthBinding
 import com.maksim.mynotes.ui.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
 
 @AndroidEntryPoint
 class AuthActivity : BaseActivity() {
@@ -15,5 +20,42 @@ class AuthActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAuthBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+    }
+
+
+    private fun encrypt() {
+        val cryptoManager = CryptoManager()
+
+        val message =  "maksim novikov, born in 1993 currently lived in Israel, works at Upnlugged company."
+        val encrypted = cryptoManager.encrypt(message)
+
+       /* val file = File(filesDir, "secret.txt")
+        if (!file.exists())
+            file.createNewFile()
+
+        val fos = FileOutputStream(file)
+
+        val encryptResult = cryptoManager.encrypt(
+            "maksim novikov, born in 1993 currently lived in Israel, works at Upnlugged company.".toByteArray(),
+            fos
+        ).decodeToString()*/
+
+        Log.d("CryptoManager", "encryptResult -> $encrypted")
+    }
+
+    private fun decrypt(data: String){
+        val cryptoManager = CryptoManager()
+
+        val decryptResult = cryptoManager.decrypt(data)
+
+       /* val file = File(filesDir, "secret.txt")
+        if (!file.exists())
+            return
+
+        val fos = FileInputStream(file)
+        val decryptedResult = cryptoManager.decrypt(fos).decodeToString()*/
+        Log.d("CryptoManager", "decryptedResult -> $decryptResult")
+
     }
 }
